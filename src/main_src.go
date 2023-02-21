5566646995109cab6e57a47473c5f057c8f4cabb
ernocobain <ernocobain89@gmail.com>
2023-02-22 00:43:32 +0700

    Html adding code

--- src/main_src.go
+++ src/main_src.go
@@ -1,13 +1,13 @@
 package src
 
 import (
-	"crypto/tls"
+	// "crypto/tls"
 	"log"
 
 	"dhikrama.com/web/src/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
-	"golang.org/x/crypto/acme/autocert"
+	// "golang.org/x/crypto/acme/autocert"
 )
 
 func MainSrc() {
@@ -17,33 +17,35 @@ func MainSrc() {
 		ViewsLayout: "layouts/main",
 	})
 
-	m := &autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		// Replace with your domain
-		HostPolicy: autocert.HostWhitelist("13.250.14.237"),
-		// Folder to store the certificates
-		Cache: autocert.DirCache("./certs"),
-	}
+	// m := &autocert.Manager{
+	// 	Prompt: autocert.AcceptTOS,
+	// 	// Replace with your domain
+	// 	HostPolicy: autocert.HostWhitelist("13.250.14.237"),
+	// 	// Folder to store the certificates
+	// 	Cache: autocert.DirCache("./certs"),
+	// }
 
-	cfg := &tls.Config{
-		// Get Certificate from Let's Encrypt
-		GetCertificate: m.GetCertificate,
-		// By default NextProtos contains the "h2"
-		// This has to be removed since Fasthttp does not support HTTP/2
-		// Or it will cause a flood of PRI method logs
-		// http://webconcepts.info/concepts/http-method/PRI
-		NextProtos: []string{
-			"http/1.1", "acme-tls/1",
-		},
-	}
-	ln, err := tls.Listen("tcp", ":443", cfg)
-	if err != nil {
-		panic(err)
-	}
+	// cfg := &tls.Config{
+	// 	// Get Certificate from Let's Encrypt
+	// 	GetCertificate: m.GetCertificate,
+	// 	// By default NextProtos contains the "h2"
+	// 	// This has to be removed since Fasthttp does not support HTTP/2
+	// 	// Or it will cause a flood of PRI method logs
+	// 	// http://webconcepts.info/concepts/http-method/PRI
+	// 	NextProtos: []string{
+	// 		"http/1.1", "acme-tls/1",
+	// 	},
+	// }
+	// ln, err := tls.Listen("tcp", ":443", cfg)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	app.Static("/", "./public/static")
 	routes.RoutesHtml(app)
 	routes.RoutersApi(app)
 
-	log.Fatal(app.Listener(ln))
+	// log.Fatal(app.Listener(ln))
+
+	log.Fatal(app.Listen(":8080"))
 }
